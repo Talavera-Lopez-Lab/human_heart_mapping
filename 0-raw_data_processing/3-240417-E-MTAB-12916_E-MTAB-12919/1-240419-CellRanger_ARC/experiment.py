402609ea8d667cd20f625eb7ed84238920a0eb63
egerc <eger.christian@gmx.de>
2024-04-22 16:59:10 +0200

    .

--- 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/experiment.py
+++ 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/experiment.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
-
+import csv
+import subprocess
+import shutil
 
 class Multiome_ATAC_Experiment():
 
@@ -33,8 +35,10 @@ class Multiome_ATAC_Experiment():
         self.cellranger_reference = os.path.join(self.data_dir, cellranger_reference)
         self.mapping_output = os.path.join(self.data_dir, mapping_output)
         self.meta_data = self.create_meta_data()
+        self.runs = self.meta_data.index.unique()
 
     def create_meta_data(self):
+
         return pd.concat(
             [
                 pd.read_table(self.rna_meta_path, index_col=self.meta_data_index),
@@ -45,6 +49,49 @@ class Multiome_ATAC_Experiment():
         )
 
     def mapping(self):
-        pass
 
+        def write_csv(csv_file, data, mode):
+            with open(csv_file, mode=mode, newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(data)
 
+        for run in self.runs:
+            run_dir = os.path.join(self.mapping_output, run)
+            os.makedirs(run_dir, exist_ok=True)
+            download_df = self.meta_data[self.meta_data.index == run]
+            libraries_colums = [['fastqs', 'sample', 'library_type']]
+            libraries_filepath = os.path.join(run_dir, 'libraries.csv')
+            write_csv(libraries_filepath, libraries_colums, mode='w')
+            for key in self.keys:
+                sample = download_df[key][self.sample_col][0]
+                sample_dir = os.path.join(run_dir, sample)
+                os.makedirs(sample_dir, exist_ok=True)
+                libraries_values = [[sample_dir, 
+                                     sample, 
+                                     'Gene Expression' if key == self.keys[0] else 'Chromatin Accessibility' if key == self.keys[1] else None]]
+                write_csv(libraries_filepath, libraries_values, mode='a')
+                file_dict = {row[self.file_name_col]: row[self.url_col] for index, row in download_df[key].iterrows()}
+                for file_name, url in file_dict.items():
+                    file_path = os.path.join(sample_dir, file_name)
+                    subprocess.run(['axel', '-n', '10', '--output', file_path, url],
+                                check=True)
+
+                # Running Cellranger
+            cellranger_arc_run = [
+                self.cellranger_dir,
+                'count',
+                '--id=' + run,
+                '--reference=' + self.cellranger_reference,
+                '--libraries=' + libraries_filepath,
+                '--localcores=20',
+                '--localmem=32'
+            ]
+            subprocess.run(cellranger_arc_run)
+            for key in self.keys:
+                sample = download_df[key][self.sample_col][0]
+                sample_dir = os.path.join(run_dir, sample)
+                try:
+                    shutil.rmtree(sample_dir)
+                    print(f'{key} sample folder deleted')
+                except OSError as e:
+                    print(f"Error: {sample_dir} : {e.strerror}")
--- 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/mapping_script.py
+++ 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/mapping_script.py
@@ -1,2 +1,29 @@
-import Multiome_ATAC_Experiment
+from experiment import Multiome_ATAC_Experiment
 
+
+rna_meta_path = 'meta_data/E-MTAB-12916.sdrf.txt'
+atac_meta_path = 'meta_data/E-MTAB-12919.sdrf.txt'
+meta_data_index = 'Description'
+sample_col = 'Source Name'
+file_name_col = 'Scan Name'
+url_col = 'Comment[FASTQ_URI]'
+parent_dir = '/mnt/LaCIE/ceger/Projects/human_heart_mapping/human_heart_mapping/0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/'
+cellranger_dir = '/home/ceger/CellRanger/cellranger-arc-2.0.2/cellranger-arc'
+cellranger_reference = 'cr_arc_index/GRCh38'
+mapping_output = 'mapping_py'
+
+multiome_experiment = Multiome_ATAC_Experiment(
+    rna_meta_path,
+    atac_meta_path,
+    meta_data_index,
+    sample_col,
+    file_name_col,
+    url_col,
+    parent_dir,
+    cellranger_dir,
+    cellranger_reference,
+    mapping_output,
+)
+
+
+multiome_experiment.mapping()
