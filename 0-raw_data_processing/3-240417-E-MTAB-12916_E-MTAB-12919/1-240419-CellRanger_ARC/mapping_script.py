5bdd7e1936c4ac530310851f62861e4f77512855
egerc <eger.christian@gmx.de>
2024-05-07 11:58:56 +0200

    rerun

--- 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/mapping_script.py
+++ 0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC/mapping_script.py
@@ -7,20 +7,11 @@ meta_data_index = 'Description'
 sample_col = 'Source Name'
 file_name_col = 'Scan Name'
 url_col = 'Comment[FASTQ_URI]'
-parent_dir = '/mnt/LaCIE/ceger/Projects/human_heart_mapping/human_heart_mapping/0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/'
+parent_dir = '/mnt/LaCIE/ceger/Projects/human_heart_mapping/human_heart_mapping/0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC'
 cellranger_dir = '/home/ceger/CellRanger/cellranger-arc-2.0.2/cellranger-arc'
 cellranger_reference = 'cr_arc_index/GRCh38'
 mapping_output = 'mapping_py'
-mapped_samples = [
-    'HCAHeart9845431_HCAHeart9917173',
-    'HCAHeart9845432_HCAHeart9917174',
-    'HCAHeart9508627_HCAHeart9508819',
-    'HCAHeart9845434_HCAHeart9917176',
-    'HCAHeart9845435_HCAHeart9917177',
-    'HCAHeart9845433_HCAHeart9917175',
-    'HCAHeart9508628_HCAHeart9508820',
-    'HCAHeart9508629_HCAHeart9508821'
-]
+mapped_samples = []
 
 multiome_experiment = Multiome_ATAC_Experiment(
     rna_meta_path,
