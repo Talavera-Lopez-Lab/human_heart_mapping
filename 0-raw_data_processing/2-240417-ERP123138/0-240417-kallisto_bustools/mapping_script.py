dab4cb3f1aef9701cf15e44e4dd75033877df255
egerc <eger.christian@gmx.de>
2024-06-04 15:46:10 +0200

    created mapping script

--- 0-raw_data_processing/2-240417-ERP123138/0-240417-kallisto_bustools/mapping_script.py
+++ 0-raw_data_processing/2-240417-ERP123138/0-240417-kallisto_bustools/mapping_script.py
@@ -1,30 +1,14 @@
-from experiment import Multiome_ATAC_Experiment
+import experiment
+import os
 
 
-rna_meta_path = 'meta_data/E-MTAB-12916.sdrf.txt'
-atac_meta_path = 'meta_data/E-MTAB-12919.sdrf.txt'
-meta_data_index = 'Description'
-sample_col = 'Source Name'
-file_name_col = 'Scan Name'
-url_col = 'Comment[FASTQ_URI]'
-parent_dir = '/mnt/LaCIE/ceger/Projects/human_heart_mapping/human_heart_mapping/0-raw_data_processing/3-240417-E-MTAB-12916_E-MTAB-12919/1-240419-CellRanger_ARC'
-cellranger_dir = '/home/ceger/CellRanger/cellranger-arc-2.0.2/cellranger-arc'
-cellranger_reference = 'cr_arc_index/GRCh38'
-mapping_output = 'mapping_py'
-mapped_samples = []
-
-multiome_experiment = Multiome_ATAC_Experiment(
-    rna_meta_path,
-    atac_meta_path,
-    meta_data_index,
-    sample_col,
-    file_name_col,
-    url_col,
-    parent_dir,
-    cellranger_dir,
-    cellranger_reference,
-    mapping_output,
-    mapped_samples=mapped_samples
+GEX_Experiment = experiment.TenX_GEX_Experiment(
+    parent_dir=os.getcwd(),
+    meta_data_path='meta_data/downloads_table.csv',
+    sample_col='run_accession',
+    file_name_col='filename',
+    url_col='url',
+    mapping_output='mapping_py'
 )
 
-multiome_experiment.mapping()
+GEX_Experiment.create_kallisto_bustools_mapping(threads=16, memory=32)
