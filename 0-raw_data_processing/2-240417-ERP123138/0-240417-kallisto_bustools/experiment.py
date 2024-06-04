c3d1d85a4eeae6d44d1ad015dbb7a834531e538f
egerc <eger.christian@gmx.de>
2024-06-04 15:45:24 +0200

    added method for mapping with kallisto bustools

--- 0-raw_data_processing/2-240417-ERP123138/0-240417-kallisto_bustools/experiment.py
+++ 0-raw_data_processing/2-240417-ERP123138/0-240417-kallisto_bustools/experiment.py
@@ -3,6 +3,7 @@ import os
 import csv
 import subprocess
 import shutil
+import glob
 
 class TenX_GEX_Experiment():
 
@@ -52,93 +53,48 @@ class TenX_GEX_Experiment():
         subprocess.run(kb_ref_run)
 
     def create_kallisto_bustools_mapping(self, threads=4, memory=8):
-        '''This method downloads a dataset sample wise, maps them and deletes the downloaded raw data'''
-        def download_from_ftp(filepath, url):
+        '''This method downloads a dataset sample wise, maps them using kallisto bustools 
+        and deletes the downloaded raw data'''
+        def download_from_ftp(url, filepath):
             '''Helper for downloading files from ftp servers using bash command "axel"'''
             download_sample = [
                 'axel', '-n', '10', '--output', filepath, url
             ]
             subprocess.run(download_sample)
-        def run_kallisto_mapping(sample):
+        def run_kallisto_mapping(sample_dir):
             '''Helper for running the mapping process'''
+            input_files = glob.glob(f'{sample_dir}/*.fastq.gz')
             kb_count_run = [
                 'kb', 'count',
-                '-i', f'{self.index_output_path}transcriptome.idx',
-                '-g', f'{self.index_output_path}transcripts_to_genes.txt',
+                '-i', os.path.join(self.index_output_path, 'transcriptome.idx'),
+                '-g', os.path.join(self.index_output_path, 'transcripts_to_genes.txt'),
                 '-x', '10XV3',
                 '--h5ad',
                 '--workflow', 'lamanno',
                 '--verbose',
                 '-t', f'{threads}',
                 '-m', f'{memory}',
-                '-c1', f'{self.index_output_path}cdna_transcripts_to_capture.txt',
-                '-c2', f'{self.index_output_path}intron_transcripts_to_capture.txt',
-                f'{os.path.join(self.mapping_output, sample)}*.fastq.gz',
-                '-o', f'{os.path.join(self.mapping_output, sample)}'
+                '-c1', os.path.join(self.index_output_path, 'cdna_transcripts_to_capture.txt'),
+                '-c2', os.path.join(self.index_output_path, 'intron_transcripts_to_capture.txt'),
+            ] + input_files + [
+                '-o', f'{sample_dir}'
             ]
             subprocess.run(kb_count_run)
 
-
-        for sample in self.runs:
-            download_df = self.meta_data.loc[sample]
-        for sample, data in self.meta_data.iterrows():
-            sample_dir = os.path.join(self.mapping_output, sample)
-            os.makedirs(sample_dir, exist_ok=True)
-            filepath = os.path.join(sample_dir, data[self.file_name_col])
-            url = data[self.url_col]
-
-    def mapping(self):
-
-        def write_csv(csv_file, data, mode):
-            with open(csv_file, mode=mode, newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows(data)
-
-        for run in self.runs:
-            run_dir = os.path.join(self.mapping_output, run)
-            os.makedirs(run_dir, exist_ok=True)
-            download_df = self.meta_data[self.meta_data.index == run]
-            libraries_colums = [['fastqs', 'sample', 'library_type']]
-            libraries_filepath = os.path.join(run_dir, 'libraries.csv')
-            write_csv(libraries_filepath, libraries_colums, mode='w')
-            for key in self.keys:
-                sample = download_df[key][self.sample_col][0]
-                sample_dir = os.path.join(run_dir, sample)
+        for outer_sample in self.runs:
+            download_df = self.meta_data.loc[outer_sample]
+            sample_dir = os.path.join(self.mapping_output, outer_sample)
+            for inner_sample, data in download_df.iterrows():
                 os.makedirs(sample_dir, exist_ok=True)
-                libraries_values = [[sample_dir, 
-                                     sample, 
-                                     'Gene Expression' if key == self.keys[0] else 'Chromatin Accessibility' if key == self.keys[1] else None]]
-                write_csv(libraries_filepath, libraries_values, mode='a')
-                file_dict = {row[self.file_name_col]: row[self.url_col] for index, row in download_df[key].iterrows()}
-                for file_name, url in file_dict.items():
-                    file_path = os.path.join(sample_dir, file_name)
-                    subprocess.run(['axel', '-n', '10', '--output', file_path, url],
-                                check=True)
+                filepath = os.path.join(sample_dir, data[self.file_name_col])
+                url = data[self.url_col]
+                download_from_ftp(url, filepath)
+            raw_files = glob.glob(f'{sample_dir}/*.fastq.gz')
+            # running the kallisto mapping algorithm
+            run_kallisto_mapping(sample_dir)
+            # Deleting the downloaded Raw fastqs
+            for file in raw_files:
+                if os.path.exists(file):
+                    os.remove(file)
+            raw_files = []
 
-                # Running Cellranger
-            cellranger_arc_run = [
-                self.cellranger_dir,
-                'count',
-                '--id=' + run,
-                '--reference=' + self.cellranger_reference,
-                '--libraries=' + libraries_filepath,
-                '--localcores=32',
-                '--localmem=128'
-            ]
-            subprocess.run(cellranger_arc_run)
-
-            for file in os.listdir(run):
-                if file.endswith('.bam'):
-                    bam_path = os.path.join(run, file)
-                    os.remove(bam_path)
-
-            for key in self.keys:
-                sample = download_df[key][self.sample_col][0]
-                sample_dir = os.path.join(run_dir, sample)
-                try:
-                    shutil.rmtree(sample_dir)
-                    print(f'{key} sample folder deleted')
-                except OSError as e:
-                    print(f"Error: {sample_dir} : {e.strerror}")
-
-            shutil.move(run, run_dir)
